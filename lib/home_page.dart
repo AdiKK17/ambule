6b7f421fe088c60a88830daf5fbb2306eee526aa
AdiKK17 <kavikmr9@gmail.com>
2020-02-26 00:17:34 +0530

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:provider/provider.dart';
+import 'package:location/location.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'auth_provider.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _HomePage();
+  }
+}
+
+class _HomePage extends State<HomePage> {
+  var currentLocation;
+  var coordinates;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    _getLocation();
+    super.initState();
+  }
+
+  Future<void> _launchURL() async {
+    final url = 'https://www.google.com/maps/dir///@$coordinates,15z';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<void> _getLocation() async {
+    var currentLocation;
+
+    try {
+      currentLocation = await Location().getLocation();
+      coordinates = "${currentLocation.latitude.toString()} , ${currentLocation.longitude.toString()}";
+      print(currentLocation.latitude);
+      print(currentLocation.longitude);
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+      }
+      currentLocation = null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final model = Provider.of<Auth>(context);
+
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(model.userEmail),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepOrange,
+        child: Icon(Icons.send),
+        onPressed: () => _launchURL()
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () {},
+                    child: Text("click"),
+                  ),
+                  RaisedButton(
+                    onPressed: () {},
+                    child: Text("click"),
+                  )
+                ],
+              ),
+              SizedBox(height: 50,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () {},
+                    child: Text("click"),
+                  ),
+                  RaisedButton(
+                    onPressed: () {},
+                    child: Text("click"),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
